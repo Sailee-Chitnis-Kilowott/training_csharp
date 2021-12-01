eb5f20acdb5d990affed987cd6456e97a8688a0a
Sailee-Chitnis-Kilowott <92842005+Sailee-Chitnis-Kilowott@users.noreply.github.com>
2021-12-01 16:01:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNetAssignment
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        int n = 15;
+        static void Main(string[] arg)
+        {
+            var spObject = new Stats();
+            spObject.Numbers = data;
+            var mean = spObject.CalculateMean();
+            Console.WriteLine($"Mean : {mean}");
+            var median = spObject.CalculateMedian();
+            Console.WriteLine($"Median : {median}");
+            var mode = spObject.CalculateMode();
+            Console.WriteLine($"Mode : {mode}");
+
+        }
+
+
+    }
+
+}
