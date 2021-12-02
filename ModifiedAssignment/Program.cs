9dcaaa4ae859cf7aff0cccf574b0a20f07967440
Sailee-Chitnis-Kilowott <92842005+Sailee-Chitnis-Kilowott@users.noreply.github.com>
2021-12-02 13:40:16 +0530

    Add files via upload

---
+++ ModifiedAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModifiedAssignment
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        int n = 15;
+        static void Main(string[] arg)
+        {
+            var spObject = new Stats();
+            spObject.Numbers = data;
+            var result = spObject.GetStatsResult();
+           
+            Console.WriteLine($"Mean : {result.Mean}");
+            Console.WriteLine($"Median : {result.Median}");
+            Console.WriteLine($"Mode : {result.Mode}");
+        }
+
+
+    }
+
+}
---
+++ ModifiedAssignment/StatsResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedAssignment
+{
+    internal class StatsResult
+    {
+        public decimal Mean { get; set; }
+        public decimal Median { get; set; }
+        public int Mode { get; set; }
+
+    }
+}
