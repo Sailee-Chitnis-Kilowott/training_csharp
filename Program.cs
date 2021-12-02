fdc2f8920e6124072b4409615f48996b72e19195
Sailee-Chitnis-Kilowott <92842005+Sailee-Chitnis-Kilowott@users.noreply.github.com>
2021-12-02 12:32:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModifiedAssignment
+{
+    internal class Program
+    {
+        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+        int n = 15;
+        static void Main(string[] arg)
+        {
+            var spObject = new Stats();
+            spObject.Numbers = data;
+            var result = spObject.GetStatsResult();
+            //var mean = spObject.CalculateMean();
+            // Console.WriteLine($"Mean : {mean}");
+            // var median = spObject.CalculateMedian();
+            // Console.WriteLine($"Median : {median}");
+            // var mode = spObject.CalculateMode();
+            // Console.WriteLine($"Mode : {mode}");
+            Console.WriteLine($"Mean : {result.Mean}");
+            Console.WriteLine($"Median : {result.Median}");
+            Console.WriteLine($"Mode : {result.Mode}");
+        }
+
+
+    }
+
+}
---
+++ Stats.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedAssignment
+{
+    internal class Stats
+    {
+        internal int[] Numbers { private get; set; }
+
+        internal StatsResult GetStatsResult()
+        { 
+            var result = new StatsResult();
+            result.Mean = CalculateMean();
+            result.Median = CalculateMedian();
+            result.Mode= CalculateMode();
+            return result;
+        }
+
+        //To calculate mean
+        internal decimal CalculateMean()
+        {
+            int i;
+            int sum = 0;
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                sum = sum + Numbers[i];
+            }
+            return (decimal)sum / Numbers.Length;
+
+        }
+        //To calculate median
+        internal decimal CalculateMedian()
+        {
+            int i, j, temp;
+
+
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                for (j = i + 1; j < Numbers.Length; j++)
+                {
+                    if (Numbers[i] > Numbers[j])
+                    {
+                        temp = Numbers[i];
+                        Numbers[i] = Numbers[j];
+                        Numbers[j] = temp;
+                    }
+                }
+
+
+            }
+            if (15 % 2 == 0)
+                return (Numbers[(15 - 1) / 2] + Numbers[15 / 2]) / 2;
+
+            else
+                return Numbers[15 / 2];
+
+        }
+
+        //To calculate mode
+        internal int CalculateMode()
+        {
+
+            int i, j, count, maxCount = 0, maxValue = 0;
+
+
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                count = 0;
+                for (j = 0; j < Numbers.Length; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxValue = Numbers[i];
+                }
+
+            }
+            return maxValue;
+        }
+    }
+}
